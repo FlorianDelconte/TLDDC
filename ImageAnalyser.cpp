addafe0d3d4b1a9d2da2a89bf4f17c1870229464
Dooz <florian.delconte@protonmail.com>
2020-04-21 14:49:29 +0200

    ajout d'une fonction permettant d'afficher le point max distance orthogonal

--- ImageAnalyser.cpp
+++ ImageAnalyser.cpp
@@ -26,29 +26,32 @@ ImageAnalyser::onMouse( int event, int x, int y, int flags, void* param)
     if( event != cv::EVENT_LBUTTONDOWN )
         return;
     cv::Point seed = cv::Point(x,y);
+    std::cout<<"x:"<<seed.x<<"y: "<<seed.y<<std::endl;
     ImageAnalyser *p_ianalyser=(ImageAnalyser*) param;
     std::vector<unsigned int > indInCells=p_ianalyser->unrolled_map.getIndPointsInLowerResolution(seed.y,seed.x,1);
-    std::cout<<"nb points in cells : "<<indInCells.size()<<std::endl;
-
+    std::cout<<"nb points in cells "<<indInCells.size()<<std::endl;
+    
     if(!indInCells.empty()){
         //delete old .dat and png
         system("rm ../clickedPatchOutput/*.dat");
         system("rm ../clickedPatchOutput/*.png");
-        unsigned int IndP;
+        unsigned int IndP,indLine;
         std::ofstream dataFile;
         CylindricalPoint mpFound;
         unsigned int nbPatches=indInCells.size();
         std::string datafilename;
-        //create new .dat files
+        double a,b;
+        //fill  patches.dat files
         for (unsigned int idPatches = 0; idPatches < nbPatches; ++idPatches){
-            datafilename="../clickedPatchOutput/data"+std::to_string(idPatches)+".dat";
+           
+            datafilename="../clickedPatchOutput/patche"+std::to_string(idPatches)+".dat";
             dataFile.open(datafilename);
             dataFile<<"#X\tY\n";
             for(std::vector<unsigned int>::iterator it = std::begin(p_ianalyser->ind_Patches[indInCells[idPatches]]); it != std::end(p_ianalyser->ind_Patches[indInCells[idPatches]]); ++it) {
                 IndP=*it;
+                
                 mpFound = p_ianalyser->CPoints.at(IndP);
                 dataFile << std::fixed << std::setprecision(8) <<mpFound.height<<"\t"<<mpFound.radius <<"\n";
-                
             }
             dataFile.close();
         }
@@ -56,18 +59,62 @@ ImageAnalyser::onMouse( int event, int x, int y, int flags, void* param)
         FILE *fp;
         fp = popen("cd ../clickedPatchOutput/; gnuplot","w");
         fprintf(fp,"set terminal png\n");
-        fprintf(fp,"set xlabel \"x\"\n");
-        fprintf(fp,"set ylabel \"y\"\n");
+        fprintf(fp,"set xlabel \"z (mm)\"\n");
+        fprintf(fp,"set ylabel \"distance to the centerline (mm)\"\n");
+        CylindricalPoint shiftedPoint;
         for (unsigned int idPatches = 0; idPatches < nbPatches; ++idPatches){
+            IndP=indInCells[idPatches];
+            mpFound = p_ianalyser->CPoints.at(IndP);
+            //shiftedPoint=p_ianalyser->getShiftedPoint(p_ianalyser->ind_CoefsLines.at(IndP),p_ianalyser->ind_Patches.at(IndP));
+            //std::cout<<"h:"<<mpFound.height<<"r: "<<mpFound.radius<<std::endl;
             fprintf(fp,"set output \"patch%d.png\"\n",idPatches);
-            fprintf(fp, "plot \"data%d.dat\" using 1:2 title \"patch%d\"\n",idPatches,idPatches);
+
+            fprintf(fp,"a=%f\n", p_ianalyser->ind_CoefsLines.at(IndP).first);
+            fprintf(fp,"b=%f\n", p_ianalyser->ind_CoefsLines.at(IndP).second);
+            fprintf(fp,"f(x) = a*x + b\n");
+
+            fprintf(fp, "plot \"patche%d.dat\" using 1:2 title \"points in the patch\",  ",idPatches,idPatches);
+            fprintf(fp, "\"<echo '%f %f'\" pt 7 ps 2 title \"processing point\",",mpFound.height,mpFound.radius);
+            //fprintf(fp, "\"<echo '%f %f'\" pt 7 ps 3 title \"farest poit\",",shiftedPoint.height,shiftedPoint.radius);
+            fprintf(fp, "f(x) title 'fitted line' with lines linestyle 1 lc 4\n");
         }
         pclose(fp);
     }
     
     
 }
-
+CylindricalPoint
+ImageAnalyser::getShiftedPoint(std::pair<double, double>& coef,std::vector<unsigned int> pointsInPatch){
+    unsigned int shiftedPoint;
+    CylindricalPoint mp;
+    CylindricalPoint maxP;
+    double A=coef.first;
+    double B=-1;
+    double C=coef.second;
+    double distanceLine;
+    //loop on points in current
+    //std::cout<<" a : "<<A<<std::endl;
+    //std::cout<<" b : "<<B<<std::endl;
+    //std::cout<<" c : "<<C<<std::endl;
+    
+    double maxdistance=INT_MIN;
+    for(std::vector<unsigned int>::iterator it = std::begin(pointsInPatch); it != std::end(pointsInPatch); ++it) {
+        mp = CPoints.at(*it);
+        //yp < f(xp)
+        if(mp.radius - (coef.first*mp.height + coef.second) < 0 ){
+            //std::cout<<" point at "<<mp.height<<" ; " << mp.radius<<"is below the fitted line"<<std::endl;
+            distanceLine=abs((A*mp.height)+(B*mp.radius)+C)/sqrt((A*A)+(B*B));
+            if(distanceLine>maxdistance){
+                maxdistance=distanceLine;
+                maxP=mp;
+            }
+        }
+    }
+    //std::cout<<" max distance is  : "<<maxdistance<<std::endl;
+    //std::cout<<" attaint par le point : ["<<maxP.height<<" ; "<<maxP.radius<<" ] "<<std::endl;
+    coef.second-=maxdistance;
+    return maxP;
+}
 void
 ImageAnalyser::analyse(){
     cv::Mat image=unrolled_map.getRgbImage();
--- ImageAnalyser.h
+++ ImageAnalyser.h
@@ -12,12 +12,14 @@ class ImageAnalyser{
     /**
     Constructor.
    **/
-    ImageAnalyser(UnrolledMap uM, std::vector<std::vector<unsigned int>> pR,std::vector<CylindricalPoint> cP):unrolled_map(uM),ind_Patches(pR),CPoints(cP)
+    ImageAnalyser(UnrolledMap uM, std::vector<std::vector<unsigned int>> pR,std::vector<CylindricalPoint> cP, std::vector<std::pair<double, double> > coefs):unrolled_map(uM),ind_Patches(pR),CPoints(cP),ind_CoefsLines(coefs)
     {}
     /**
     display rgb image from unrolled map and allow user event
     **/
     void analyse();
+
+    CylindricalPoint getShiftedPoint(std::pair<double, double>& coef,std::vector<unsigned int> pointsInPatch);
     /**
     menber function to setMouseCallBack
     **/
@@ -32,6 +34,8 @@ class ImageAnalyser{
     UnrolledMap unrolled_map;
     
     std::vector<CylindricalPoint> CPoints;
+
+    std::vector<std::pair<double, double> > ind_CoefsLines;
 };
 
 #endif
